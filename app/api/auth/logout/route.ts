5c1e1c3b0e584593f3fd54701a993593e52f71ed
Vitalia <vitaliya.m05@gmail.com>
2025-07-17 22:54:30 +0200

    api updated

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import { api } from "../../api";
 import { cookies } from "next/headers";
+import { isAxiosError } from "axios";
+import { logErrorResponse } from "../../_utils/utils";
 
 export async function POST() {
-  const cookieStore = await cookies();
-
   try {
+    const cookieStore = await cookies();
+
     const accessToken = cookieStore.get("accessToken")?.value;
     const refreshToken = cookieStore.get("refreshToken")?.value;
 
@@ -18,9 +20,22 @@ export async function POST() {
     cookieStore.delete("accessToken");
     cookieStore.delete("refreshToken");
 
-    return NextResponse.json({ message: "Logged out successfully" });
+    return NextResponse.json(
+      { message: "Logged out successfully" },
+      { status: 200 }
+    );
   } catch (error) {
-    console.error("Logout failed:", error);
-    return NextResponse.json({ error: "Logout failed" }, { status: 500 });
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
